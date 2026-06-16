d145e73df6d90ce91416dddaad40a8fa9e0399e3
keishun <c0a2525296@edu.teu.ac.jp>
2026-06-16 14:57:41 +0900

    練習問題3

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,60 +1,70 @@
 import os
+import random
 import sys
 import pygame as pg
-import random
+
 
 WIDTH, HEIGHT = 1100, 650
+DELTA = {
+    pg.K_UP: (0, -5),  
+    pg.K_DOWN: (0, +5),
+    pg.K_LEFT: (-5, 0),  
+    pg.K_RIGHT: (+5, 0), 
+}
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
+
+def check_bound(rct: pg.Rect) -> tuple[bool, bool]:
+    yoko, tate = True, True
+    if rct.left < 0 or WIDTH < rct.right:  # 横方向判定
+        yoko = False
+    if rct.top < 0 or HEIGHT < rct.bottom:  # 縦方向判定
+        tate = False
+    return yoko, tate
 
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
     bg_img = pg.image.load("fig/pg_bg.jpg")    
-    bg_img_flip = pg.transform.flip(bg_img, True, False)
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
+
+    bb_img = pg.Surface((20, 20)) 
+    pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10) 
+    bb_img.set_colorkey((0, 0, 0))
+    bb_rct = bb_img.get_rect() 
+    bb_rct.centerx = random.randint(0, WIDTH) 
+    bb_rct.centery = random.randint(0, HEIGHT) 
+    vx, vy = +5, +5
+
     clock = pg.time.Clock()
     tmr = 0
-
-    bb_img = pg.Surface((20, 20))
-    pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)
-    bb_img.set_colorkey((0, 0, 0))
-
-    bb_rct = bb_img.get_rect()
-    bb_rct.center = (
-    random.randint(0, 1100),
-    random.randint(0, 650)
-    )   
-
-    vx, vy = 5, 5
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
-        x = tmr % 3200
-
-        screen.blit(bg_img, (-x, 0))
-        screen.blit(bg_img_flip, (-x + 1600, 0))
-        screen.blit(bg_img, (-x + 3200, 0))
         screen.blit(bg_img, [0, 0]) 
 
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
-        if key_lst[pg.K_UP]:
-            sum_mv[1] -= 5
-        if key_lst[pg.K_DOWN]:
-            sum_mv[1] += 5
-        if key_lst[pg.K_LEFT]:
-            sum_mv[0] -= 5
-        if key_lst[pg.K_RIGHT]:
-            sum_mv[0] += 5
+        for key, mv in DELTA.items():
+            if key_lst[key]:
+                sum_mv[0] += mv[0]  
+                sum_mv[1] += mv[1] 
         kk_rct.move_ip(sum_mv)
-        bb_rct.move_ip(vx, vy)
-        screen.blit(bb_img, bb_rct)
+        if check_bound(kk_rct) != (True, True):
+            kk_rct.move_ip(-sum_mv[0], -sum_mv[1])  
         screen.blit(kk_img, kk_rct)
+
+        bb_rct.move_ip(vx, vy)
+        yoko, tate = check_bound(bb_rct)
+        if not yoko:  # 横方向にはみ出ていたら
+            vx *= -1
+        if not tate:  # 縦方向にはみ出ていたら
+            vy *= -1
+        screen.blit(bb_img, bb_rct)
         pg.display.update()
         tmr += 1
         clock.tick(50)
